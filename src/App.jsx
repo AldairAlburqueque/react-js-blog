0026ad351aee5bf2234551b862e48890ab90b927
AldairAlburqueque <aldairalburqueque8@gmail.com>
2026-02-14 20:33:49 -0500

    add to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <>
-      <HomePage></HomePage>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="name">Name</label>
+        <input type="text" />
+
+        <label htmlFor="email">Email</label>
+        <input type="text" />
+
+        <label htmlFor="description">Description</label>
+        <input type="text" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" />
+
+        <label htmlFor="name">name</label>
+        <input type="text" />
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
